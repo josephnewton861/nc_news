53062cb7cbe3523e41906bbb615c5640b6f7c387
Joe <josephnewton861@yahoo.co.uk>
2020-04-14 08:42:25 +0100

    First commit of review

---
+++ db/data/development-data/index.js
@@ -0,0 +1,5 @@
+exports.articlesData = require('../development-data')
+exports.commentsData = require('../development-data/comments')
+exports.topicsData = require('../development-data/topics')
+exports.usersData = require('../development-data/users')
+
---
+++ db/data/test-data/index.js
@@ -0,0 +1,4 @@
+exports.articlesData = require('../test-data/articles')
+exports.commentsData = require('../test-data/articles')
+exports.topicsData = require('../test-data/topics')
+exports.usersData = require('../test-data/users')
