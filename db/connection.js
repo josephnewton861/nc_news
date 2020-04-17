ca0fe0f3647d190b63d0ef8a5f2b87c3bac6a1aa
Joe <josephnewton861@yahoo.co.uk>
2020-04-17 15:57:55 +0100

    Reformated connection js file

--- db/connection.js
+++ db/connection.js
@@ -1,15 +1,7 @@
-// const knex = require("knex");
-// const customConfig = require("../knexfile");
-// const connection = knex(customConfig);
-// module.exports = connection;
-
-
-const ENV = process.env.NODE_ENV || 'development';
-const knex = require('knex');
-​
+const ENV = process.env.NODE_ENV || "development";
+const knex = require("knex");
 const dbConfig =
-  ENV === 'production'
-    ? { client: 'pg', connection: process.env.DATABASE_URL }
-    : require('../knexfile');
-​
+  ENV === "production"
+    ? { client: "pg", connection: process.env.DATABASE_URL }
+    : require("../knexfile");
 module.exports = knex(dbConfig);
--- listen.js
+++ listen.js
@@ -1,6 +1,4 @@
-const app = require('./app')
+const app = require("./app");
 const { PORT = 9090 } = process.env;
-​
+
 app.listen(PORT, () => console.log(`Listening on ${PORT}...`));
-
-
